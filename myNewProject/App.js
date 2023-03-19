d8b15c94c3b2703dd7a020c37b00546d45f5b55c
Dima <dimsaw85@gmail.com>
2023-03-19 21:08:57 +0200

    change app

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const inputHandler = (text) => setValue(text);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Type text"
+          value={value}
+          onChangeText={inputHandler}
+        />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 30
   },
 });
+
+
+
+// // import { StatusBar } from "expo-status-bar";
+// // import { StyleSheet, Text, View } from "react-native";
+
+// // export default function App() {
+// //   return (
+// //     <View style={styles.container}>
+// //       <Text> I LOVE YULIIA!</Text>
+// //       <StatusBar style="auto" />
+// //     </View>
+// //   );
+// // }
+
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     backgroundColor: "#fff",
+// //     alignItems: "center",
+// //     justifyContent: "center",
+// //   },
+// // });
+
+
+
+
+
+// import React, { useState } from "react";
+// import {
+//   StyleSheet,
+//   View,
+//   TextInput,
+// } from "react-native";
+
+// export default function App() {
+//   const [value, setValue] = useState("");
+//   const inputHandler = (text) => setValue(text);
+//   return (
+//       <View style={styles.container}>
+//         <TextInput
+//           placeholder="Type text"
+//           value={value}
+//           onChangeText={inputHandler}
+//         />
+//       </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "flex-end",
+//     paddingBottom: 30
+//   },
+// });
+
