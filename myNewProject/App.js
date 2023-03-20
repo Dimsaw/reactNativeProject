ecb87f09df66af17775a1a1e64b4eaf8cb8da1b7
Dima <dimsaw85@gmail.com>
2023-03-20 13:58:21 +0200

    registration page

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -4,13 +4,23 @@ import {
   StyleSheet,
   View,
   Text,
+  ImageBackground,
+  TextInput
 } from "react-native";
 
 export default function App() {
 
   return (
       <View style={styles.container}>
+        <ImageBackground style={styles.image}
+        source={require('./images/photoGround.jpg')}>
+          <View style={styles.menu}>
+          <TextInput style={styles.input} textAlign={'center'}/>
         <Text style={styles.text}>Start work</Text>
+          </View>
+        
+        </ImageBackground>
+        
         
       </View>
   );
@@ -20,13 +30,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 30
+
+
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    // alignItems: 'center'
+    
+    
+  },
+  menu: {
+    backgroundColor: '#fff',
+    borderColor: 'green',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: "flex-end",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+
+  input: {
+borderWidth: 1,
+marginHorizontal: 50,
+height: 40,
+borderRadius: 15,
+borderColor: 'red'
+  }, 
   text: {
-    color: "red",
-    fontSize: 50,
+    color: "black",
+    fontSize: 45,
+    textAlign: 'center',
   }
 });
 
