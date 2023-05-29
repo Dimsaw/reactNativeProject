7c98926d8272184c5c338a37145b356adce96ca4
Dima <dimsaw85@gmail.com>
2023-05-29 18:18:22 +0300

    add styles registartion

--- myNewProject/Screens/authScreen/registrationScreen/RegistrationScreen.js
+++ myNewProject/Screens/authScreen/registrationScreen/RegistrationScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 import {
-  StyleSheet,
   View,
   Text,
   Image,
@@ -16,6 +15,7 @@ import {
   Alert,
 } from "react-native";
 
+import styles from "./registartion.styled";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 
@@ -318,106 +318,6 @@ function Registration({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    fontFamily: "Roboto-Regular",
-  },
-  containerMenu: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
 
-    justifyContent: "flex-end",
-  },
-
-  imageThumb: {
-    top: -60,
-    position: "absolute",
-    width: 120,
-    height: 120,
-    backgroundColor: "#F6F6F6",
-    borderRadius: 16,
-  },
-  addButton: {
-    position: "absolute",
-    top: 21,
-    width: 25,
-    height: 25,
-  },
-  avatarImage: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 16,
-    resizeMode: "cover",
-  },
-
-  menu: {
-    backgroundColor: "#FFFFFF",
-    borderTopRightRadius: 25,
-    borderTopLeftRadius: 25,
-  },
-
-  input: {
-    borderWidth: 1,
-    height: 50,
-    borderRadius: 8,
-    backgroundColor: "#F6F6F6",
-    borderColor: "#E8E8E8",
-    marginHorizontal: 16,
-    marginBottom: 16,
-    paddingLeft: 16,
-  },
-
-  text: {
-    color: "#212121",
-    fontSize: 30,
-    textAlign: "center",
-    marginBottom: 32,
-    marginTop: 90,
-    fontFamily: "Roboto-Medium",
-  },
-  btn: {
-    borderWidth: 1,
-    height: 50,
-    borderRadius: 100,
-    backgroundColor: "#FF6C00",
-    borderColor: "#ffffff",
-    marginHorizontal: 16,
-    marginTop: 27,
-    marginBottom: 16,
-    paddingLeft: 16,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  toogleBtnPassword: {
-    position: "absolute",
-    right: 16,
-    top: 150,
-    paddingRight: 16,
-  },
-  toogleTextPassword: {
-    color: "#1B4371",
-    fontSize: 16,
-    fontFamily: "Roboto-Regular",
-  },
-  btnText: {
-    color: "#FFFFFF",
-    fontSize: 16,
-  },
-  boxBtn: {
-    paddingBottom: 79,
-  },
-  linkText: {
-    color: "#1B4371",
-    textAlign: "center",
-    fontSize: 16,
-  },
-});
 
 export default Registration;
